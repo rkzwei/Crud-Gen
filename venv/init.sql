8cb8340a18a4ae02df620cd67c6b7866e9a596f1
rk <rk@rkzwei.dev>
2023-11-01 07:13:36 -0300

    added table creation as script due to failure in code

---
+++ venv/init.sql
@@ -0,0 +1,16 @@
+-- Create the 'students' database
+CREATE DATABASE students;
+
+-- Connect to the 'students' database
+\c students;
+
+-- Create a table for students
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    nome TEXT,
+    idade INTEGER,
+    nota_primeiro_semestre FLOAT,
+    nota_segundo_semestre FLOAT,
+    nome_professor TEXT,
+    numero_sala INTEGER
+);
